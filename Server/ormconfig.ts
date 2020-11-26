f17f2dbc6d8f35eaad9a891a6a08b39324c9fadf
Rennan Cockles <rennancockles@gmail.com>
2020-11-26 12:23:36 -0300

    database init

---
+++ Server/ormconfig.ts
@@ -0,0 +1,17 @@
+export default {
+  "type": "mysql",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PWD,
+  "database": process.env.DB_DATABASE,
+  "entities": [
+     "./src/models/**/*.ts"
+  ],
+  "migrations": [
+     "./src/database/migrations/**/*.ts"
+  ],
+  "cli": {
+      "migrationsDir": "./src/database/migrations"
+  }
+}
